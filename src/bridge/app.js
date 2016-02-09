b82296b3da314ef8debea0b92eaa2cf9b167640b
Michael Kinsey <michael.kinsey@umontana.edu>
2016-02-09 11:17:59 -0700

    refactored and cleaned up app, fixed gulp dependencies

--- src/bridge/app.js
+++ src/bridge/app.js
@@ -1,26 +1,14 @@
 // wrap entire app in an enclosure
-(function(){
+(function(angular){
 
   // define module
-  var app = angular.module('orbitable', []);
+  angular.module('orbitable', [])
 
-  // test directive, draw circles
-  app.controller('TestController', function(){
-    var c = document.getElementById("main-canvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(800,600,40,0,2*Math.PI);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(1,80,40,0,2*Math.PI);
-    ctx.stroke();
-  });
-
-  app.controller('BodyController', function() {});
+  .controller('BodyController', function() {})
 
 
   // admin panel
-  app.controller('AdminController', function($scope) {
+  .controller('AdminController', function($scope) {
     $scope.admin = true;
     this.add = function() {
       console.log("add function()");
@@ -37,10 +25,10 @@
     this.record = function() {
       console.log("record function()");
     }
-  });
+  })
 
   // user functions
-  app.controller('UserController', function($scope) {
+  .controller('UserController', function($scope) {
     $scope.user = true;
 
     this.play = function() {
@@ -54,15 +42,4 @@
     }
   });
 
-  var body = {
-    x: 10,
-    y: 15,
-    x1: 5,
-    y1: 0,
-    mass: 100
-  }
-
-  test = function(){
-    return true;
-  }
-})();
+})(angular);
