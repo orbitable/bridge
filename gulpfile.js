2641c48b183f76381d702a845b83e94bc306a0b8
Benjamin Campbell <benji@benjica.com>
2016-02-12 23:01:27 -0700

    Cleanup gulpfile to add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,30 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var path = require('path');
-var plumber = require('gulp-plumber');
 var runSequence = require('run-sequence');
 var webserver = require('gulp-webserver');
 
-/**
- * File patterns
- **/
 
-// Root directory
-var rootDirectory = path.resolve('./');
-
-// Source directory for build process
-var sourceDirectory = path.join(rootDirectory, './src');
-
-// tests
-var testDirectory = path.join(rootDirectory, './test/unit');
-
+var rootDirectory = path.resolve('.');
+var sourceDirectory = path.join(rootDirectory, 'src');
+var testDirectory = path.join(rootDirectory, 'test/unit');
 var sourceFiles = [
-
-  // Make sure module files are handled first
   path.join(sourceDirectory, '/**/*.module.js'),
-
-  // Then add all JavaScript files
   path.join(sourceDirectory, '/**/*.js')
 ];
 
 gulp.task('build', function() {
   gulp.src(sourceFiles)
-    .pipe(plumber())
     .pipe(concat('bridge.js'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(uglify())
-    .pipe(rename('bridge.min.js'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('webserver', function() { 
-    gulp.src('.')
-        .pipe(webserver({
-            fallback: 'index.html',
-            open: true 
-        }
-    ));
+gulp.task('webserver', function() {
+  gulp.src(rootDirectory)
+    .pipe(webserver({fallback: 'index.html', open: true}));
 });
+
+gulp.watch('src/**/*.js', ['build']);
 
 gulp.task('default', function () {
   runSequence('build', 'webserver');
