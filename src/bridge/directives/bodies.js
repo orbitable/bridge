ac514fb9926c34ec813b1cc71d9b7b6643b4d346
Benjamin Campbell <benji@benjica.com>
2016-02-23 21:21:46 -0700

    Migrate d3 code to directive

--- src/bridge/directives/bodies.js
+++ src/bridge/directives/bodies.js
@@ -2,7 +2,7 @@
 var angular = require('angular');
 
 angular.module('bridge.directives')
-  .directive('bodies', ['$interval', 'eventPump', function($interval, eventPump) {
+  .directive('bodies', ['$interval', 'eventPump', 'simulator', function($interval, eventPump, simulation) {
     return {
       link: function(scope, elem, attr) {
         var svg = d3.select(elem[0])
@@ -15,14 +15,76 @@ angular.module('bridge.directives')
         var bodyGroup = svgGroup.append('g')
           .attr('id', 'bodyGroup');
 
-        svgGroup.append('g')
-          .attr('id', 'xAxis')
-          .attr('class', 'x-axis')
-          .attr('transform', 'translate(0,' + 0 + ')');
+        // TODO: Get dimensions from element
+        var width  = document.getElementById('svg').offsetWidth;
+        var height = document.getElementById('svg').offsetHeight;
+
+        var x = d3.scale.linear()
+          .domain([-width / 2, width / 2])
+          .range([0, width]);
+
+        var y = d3.scale.linear()
+          .domain([-height / 2, height / 2])
+          .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+          .scale(x)
+          .ticks(width/70)
+          .orient("bottom")
+          .tickSize(-height);
+
+        var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .ticks(height/70)
+          .tickSize(-width);
+
+        var zoom = d3.behavior.zoom()
+          .x(x)
+          .y(y)
+          .scaleExtent([0.1, 2])
+          .on("zoom", function() {
+            svg.select(".x.axis").call(xAxis);
+            svg.select(".y.axis").call(yAxis);
+            bodyGroup.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+          });
+
+        svg.call(zoom);
+        bodyGroup.call(zoom);
 
         svgGroup.append('g')
-          .attr('id', 'yAxis')
-          .attr("class", "y-axis");
+          .attr('id', 'xAxis')
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svgGroup.append('g')
+          .attr("class", "y axis")
+          .call(yAxis);
+
+        $interval(function () {
+          console.log(simulation.bodies);
+        }, 1000);
+
+        eventPump.register(function() {
+          bodyGroup.selectAll('*').remove();
+
+          var circle = bodyGroup.selectAll('circle').data(simulation.bodies);
+          var circleEnter = circle.enter()
+            .append("circle")
+            .attr('cx', function(d) {
+              return d === null ? 0 : d.position.x;
+            })
+            .attr('cy', function(d) {
+              return d === null ? 0 : d.position.y;
+            })
+            .attr('r', function(d) {
+              return d === null ? 0 : d.radius;
+            })
+            .attr('fill', function(d) {
+              return 'red';
+            });
+        });
       }
     };
   }]);
