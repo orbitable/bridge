26e9046b82991a533ffa132b95755d9b1a4a6b88
Benjamin Campbell <benji@benjica.com>
2016-03-26 22:19:16 -0600

    Index paths by body ID
    Problem
    -------
    
    Paths are stored by an index value. This breaks as the number of bodies in a
    simulation changes and paths are now aligned with a different body.
    
    Solution
    --------
    
    Use an object which maps a body's id from Engine to a collection of positions.
    
    How To Test
    -----------
    
    The application should have no regressions.

---
+++ src/bridge/directives/simulation/simulation.js
@@ -0,0 +1,42 @@
+var angular = require('angular');
+var d3      = require('d3');
+
+angular.module('bridge.directives')
+  .directive('simulation', function($window) {
+    return {
+      link: {
+        pre: function(scope, elem) {
+          var svgElem = elem.find('svg')[0];
+
+          scope.svg = d3.select(svgElem);
+
+          scope.height = svgElem.clientHeight;
+          scope.width = svgElem.clientWidth;
+          scope.windowCenter = [scope.width / 2, scope.height / 2];
+
+          scope.xScale = d3.scale.linear()
+            // .domain([]) // TODO: Get constraints
+            .range([0, scope.width]);
+
+          scope.yScale = d3.scale.linear()
+            // .domain([]) // TODO: Get constraints
+            .range([scope.height, 0]);
+
+          scope.zoom = d3.behavior.zoom()
+            .x(scope.xScale)
+            .y(scope.yScale)
+            .scaleExtent([0.1, 6]); // TODO: Pass scale extent as attributes
+
+          scope.svg.call(scope.zoom);
+        }
+      },
+      // TODO: Use html template
+      template: '<svg  id="svg" class="simulation">' +
+         '<g axis></g>' +
+         '<g habitable-zones></g>' +
+         '<g paths></g>' +
+         '<g bodies></g>' +
+         '<g ruler></g>' +
+       '</svg>',
+    };
+  });
