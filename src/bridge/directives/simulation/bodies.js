beb7fe0e484f76aae31ee6d3b0fcc5b6448338f2
aaroncameron21 <aaroncameron21@gmail.com>
2016-04-14 09:45:09 -0600

    Initial commit for notes

--- src/bridge/directives/simulation/bodies.js
+++ src/bridge/directives/simulation/bodies.js
@@ -10,12 +10,8 @@ angular.module('bridge.directives')
         var bodyGroup = d3.select(elem[0]);
 
         function update(data) {
-          var bodies = bodyGroup
-          .selectAll('circle')
-          .data(data);
-
-
-          function drawBodies(bodies) {
+          
+        function drawBodies(bodies) {
             bodies
             .attr('cx', (d) => scope.xScale(d.position.x))
             .attr('cy', (d) => scope.yScale(d.position.y))
@@ -49,12 +45,80 @@ angular.module('bridge.directives')
 
           }
 
+            // This draws the note markers
+            // TODO: This should only draw for admin users
+            function drawAllNotes(notes) {
+                notes
+                    .attr('x', (d) => scope.xScale(d.position.x)-16)
+                    .attr('y', (d) => scope.yScale(d.position.y)-10)
+                    .attr('height',  12)
+                    .attr('width',  12)
+                    .attr('style',"stroke:grey;stroke-width:2;fill-opacity:0.0;stroke-opacity:1.0")
+                    .on('mousedown', function(d) {
+                        d3.event.stopPropagation();
+                        scope.selectedBody = d;
+                        $('#right-sidebar').show();
+                    })
+                    .on('mouseover',function() {
+                        d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .attr('stroke', 'white')
+                        .attr('stroke-width', 3);
+                    })
+                    .on('mouseout',function() {
+                        d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .attr('stroke', 'grey')
+                        .attr('stroke-width', 1);
+                    })
+                    .on('mousedown', function(d) {
+                        d3.event.stopPropagation();
+                        scope.selectedBody = d;
+                        $('#right-sidebar').show();
+                    });
+            }
+            
+         // Draws the note information. This is when the note "pops up"
+        function drawShowNotes(notes) {
+                notes
+                    .attr('x', (d) => scope.xScale(d.position.x))
+                    .attr('y', (d) => scope.yScale(d.position.y))
+                    .attr('fill', 'white')
+                    .text( (d) =>  d.title + ": " + d.text);
+            }
+
+
+          var bodies = bodyGroup
+          .selectAll('circle')
+          .data(data.bodies);
+
           drawBodies(bodies);
           drawBodies(bodies.enter().append('circle'));
           bodies.exit().remove();
+          
+         var allNotes = bodyGroup
+          .selectAll('rect')
+          .data(data.notes);
+          
+          drawAllNotes(allNotes);
+          drawAllNotes(allNotes.enter().append('rect'));
+          allNotes.exit().remove();
+          
+          
+         var showNotes = bodyGroup
+          .selectAll('text')
+          .data(data.notes.filter(function(note) {
+              return note.check(simulator.simulationTime);
+          }));
+          
+          drawShowNotes(showNotes);
+          drawShowNotes(showNotes.enter().append('text'));
+          showNotes.exit().remove();
         }
 
-        eventPump.register(() => update(simulator.bodies));
+        eventPump.register(() => update(simulator));
       }
     };
   }]);
