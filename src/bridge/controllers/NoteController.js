a6c4af454c38529446c21d1d1c8c8f656a51519f
Benjamin Campbell <benji@benjica.com>
2016-04-25 21:28:19 -0600

    Update Angular with Updated Notes
    Problem
    -------
    
    Angular does not know when the collection of visible notes changes.
    
    Solution
    --------
    
    Watch the collection on the event loop and call $apply when the list changes.

--- src/bridge/controllers/NoteController.js
+++ src/bridge/controllers/NoteController.js
@@ -2,31 +2,41 @@ angular.module('bridge.controllers')
   .controller('noteController', ['$scope', 'eventPump',  'simulator', function($scope, eventPump, simulator) {
 
     $scope.notePause = false;
-    
-    $scope.showNotes = function() {
-      $scope.notePause = false;
-      return simulator.notes.filter(function(n) {
-        if (n.pauseSim) {
-          $scope.notePause = true;
-        }
+    $scope.notes = [];
+
+    eventPump.register(function() {
+      $scope.notePause = simulator.notes.filter((n) => n.pauseSim).length > 0;
+      var newNotes = simulator.notes.filter(function(n) {
         return n.check(simulator.simulationTime);
       });
-    };
-    
+
+      if ($scope.notes.length !== 0 && newNotes.length === 0) {
+        $scope.notes = [];
+        $scope.$apply();
+      }
+
+      newNotes.map((n) => n.id).forEach(function(n) {
+        if ($scope.notes.indexOf(n) === -1) {
+          $scope.notes = newNotes;
+          $scope.$apply();
+        }
+      });
+    });
+
     this.removeNote = function(id) {
       $('#' + id).attr('stroke-width', 0);
       simulator.deleteNote(id);
       eventPump.step(false,true);
       this.closePanel();
     };
-    
+
     this.updateNote = function() {
-        var n = $scope.selectedNote;
-        simulator.updateNote(n.id, n);
+      var n = $scope.selectedNote;
+      simulator.updateNote(n.id, n);
     };
 
     this.closePanel = function() {
       $('#note-sidebar').hide();
     };
-  
-  }])
+
+  }]);
