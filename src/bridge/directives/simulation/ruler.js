8e57a87e45ba73e75c0e6503a316cdd8000bf8f1
Benjamin Campbell <benji@benjica.com>
2016-05-04 10:08:37 -0600

    Add Units Filtering to Ruler
    Problem
    -------
    
    The Ruler does not show numerical values in terms of the selected units.
    
    Solution
    --------
    
    Use the Units service to label the tick accordingly.

--- src/bridge/directives/simulation/ruler.js
+++ src/bridge/directives/simulation/ruler.js
@@ -1,12 +1,12 @@
 var angular = require('angular');
 
 angular.module('bridge.directives')
-  .directive('ruler', ['Scale', function(Scale) {
+  .directive('ruler', ['Scale', 'Units', function(Scale, Units) {
     return {
       link: function(scope, elem) {
 
         var rulerGroup = d3.select(elem[0])
-          .attr('id', 'rulerGroup')
+          .attr('id', 'rulerGroup');
         var translation = d3.select('#translation');
 
         var rulerBtn = d3.select('#btn_ruler');
@@ -21,7 +21,7 @@ angular.module('bridge.directives')
         scope.svg.on('click', function(d) {
           if (rulerBtn.property('className').indexOf('on') > -1) {
             origPos = rulerSet ? d3.mouse(translation[0][0]) : origPos;
-            origPos = origPos.map(p => p*scope.zoom.scale())
+            origPos = origPos.map(p => p*scope.zoom.scale());
             rulerSet = !rulerSet;
           }
         });
@@ -30,38 +30,39 @@ angular.module('bridge.directives')
           if (rulerBtn.property('className').indexOf('on') > -1) {
             if (!rulerSet) {
               var pos = d3.mouse(translation[0][0]);
-              pos = pos.map(p => p*scope.zoom.scale())
+              pos = pos.map(p => p*scope.zoom.scale());
               var opposite = pos[1] - origPos[1];
               var adjacent = pos[0] - origPos[0];
               var angle = Math.atan(opposite / adjacent) / Math.PI * 180 || 0;
 
               rulerGroup.select('g').remove();
-              
+
               var x2 = Math.pow(opposite, 2);
               var y2 = Math.pow(adjacent, 2);
-              var dist = Math.sqrt(x2 + y2)/ scope.zoom.scale();
-              opposite = opposite/scope.height;
-              adjacent = adjacent/scope.height;
-              
+              var dist = Math.sqrt(x2 + y2) / scope.zoom.scale();
+              opposite = opposite / scope.height;
+              adjacent = adjacent / scope.height;
+
               var x2 = Math.pow(opposite, 2);
               var y2 = Math.pow(adjacent, 2);
               var distScaled = 1482671117702*Math.sqrt(x2 + y2)/ scope.zoom.scale();
-              
+
               var domain = [distScaled, 0];
               var range = origPos[0] > pos[0] ? [-dist, 0] : [dist, 0];
-              
-            
-              
+
               rulerScale
                 .domain(domain)
                 .range(range);
 
               rulerGroup.select('g').remove();
-              
+
+              var unitLabel = (d) => d == 0 ? '' : '' + Units.distanceFromSim(d) + ' ' + Units.labels.distance;
+
               var rulerAxis = d3.svg.axis()
                .scale(rulerScale)
                .ticks(2)
                .tickValues([0,distScaled])
+               .tickFormat(unitLabel)
                .tickSize(25);
 
               var trans = d3.transform(rulerGroup.attr('transform')).translate;
